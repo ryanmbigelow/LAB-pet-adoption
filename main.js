8d2c91e2dc014351705b3f896c06cbba72664ca6
ryanmbigelow <ryanmbigelow@gmail.com>
2022-10-16 17:44:52 -0500

    completed through step 4

--- main.js
+++ main.js
@@ -241,33 +241,35 @@ const pets = [
     }
   ];
 
-  for (let i = 0; i < pets.length; i++) {
-    console.log(pets[i].id)
-  }
+  // for (let i = 0; i < pets.length; i++) {
+  //   console.log(pets[i].id)
+  // }
 
-  for (let i = 0; i < pets.length; i++) {
-    console.log(pets[i].name)
-  }
+  // for (let i = 0; i < pets.length; i++) {
+  //   console.log(pets[i].name)
+  // }
 
-  for (let i = 0; i < pets.length; i++) {
-    console.log(pets[i].color)
-  }
+  // for (let i = 0; i < pets.length; i++) {
+  //   console.log(pets[i].color)
+  // }
 
-  for (let i = 0; i < pets.length; i++) {
-    console.log(pets[i].specialSkill)
-  }
+  // for (let i = 0; i < pets.length; i++) {
+  //   console.log(pets[i].specialSkill)
+  // }
 
-  for (let i = 0; i < pets.length; i++) {
-    console.log(pets[i].type)
-  }
+  // for (let i = 0; i < pets.length; i++) {
+  //   console.log(pets[i].type)
+  // }
 
-  for (let i = 0; i < pets.length; i++) {
-    console.log(pets[i].imageUrl)
-  }
+  // for (let i = 0; i < pets.length; i++) {
+  //   console.log(pets[i].imageUrl)
+  // }
 
 
 
 const rootDiv = document.getElementById("root");
+
+const form = document.querySelector('form');
 
 for (let i = 0; i < pets.length; i++) {
   const pet = pets[i]
@@ -285,11 +287,79 @@ for (let i = 0; i < pets.length; i++) {
   rootDiv.innerHTML += cardString;
 }
 
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
 
-//<div class="card" id="demo-card">
-//<div class="pet-name">${pet.name}</div>
-//<div class="card-image"><img src="${pet.imageUrl}"/></div>
-//<div class="pet-color">${pet.color}</div>
-//<div class="special-skill">${pet.specialSkill}</div>
-//<div class="pet-type">${pet.type}</div>
-//</div>
+const petsOnDom = (array) => {
+  let petString = "";
+  for (const pet of array) {
+    petString += `
+    <div class="card">
+    <h3 class="card-header">${pet.name}</h3>
+    <div class="card-body">
+      <img src="${pet.imageUrl}" class="card-img-bottom" alt="Photo of ${pet.name} the ${pet.type}">
+      <h5 class="card-title">${pet.color}</h5>
+      <p class="card-text">${pet.specialSkill}</p>
+    </div>
+    <h4 class="${pet.type}">${pet.type}</h4>
+  </div>
+    `
+  }
+  renderToDom("#root", petString)
+}
+
+
+
+const filter = (pets, type) => {
+  const filterPetsArray = [];
+  for (const pet of pets) {
+    if (pet.type === type) {
+      filterPetsArray.push(pet);
+    }
+  }
+  return filterPetsArray;
+}
+
+const dogButton = document.querySelector('#showDogs');
+dogButton.addEventListener('click', () => {
+  const dogs = filter(pets, 'dog');
+  petsOnDom(dogs);
+})
+
+const catButton = document.querySelector('#showCats');
+catButton.addEventListener('click', () => {
+  const cats = filter(pets, 'cat');
+  petsOnDom(cats);
+})
+
+const dinoButton = document.querySelector('#showDinos');
+dinoButton.addEventListener('click', () => {
+  const dinos = filter(pets, 'dino');
+  petsOnDom(dinos);
+})
+
+const allButton = document.querySelector('#showAll');
+allButton.addEventListener('click', () => {
+  const all = pets;
+  petsOnDom(all);
+})
+
+const newPet = (event) => {
+  event.preventDefault();
+  const petObj = {
+    id: pets.length + 1,
+    name: document.querySelector("#name").value,
+    color: document.querySelector("#color").value,
+    specialSkill: document.querySelector("#specialSkill").value,
+    type: document.querySelector("#type").value,
+    imageUrl: document.querySelector("#imageUrl").value
+  }
+  console.log('Still working')
+  pets.push(petObj);
+  petsOnDom(pets);
+  form.reset();
+}
+
+form.addEventListener('submit', newPet);
